aeb6ec8bf3eea431863889efd71aace669d53f65
Jayesh Singla <jayesh11.3.01@gmail.com>
2021-11-22 11:29:32 +0000

    Some fixes

--- branch/tage.h
+++ branch/tage.h
@@ -9,17 +9,19 @@
 #define TAGE_TAG_BITS 9
 #define TAGE_HISTORY_LENGTH 500
 
+struct tage_predictor_table_entry{
+    uint8_t ctr;  // range 0-7
+    uint16_t tag; 
+    uint8_t useful;  // range 0-3
+    
+};
+
 class Tage
 {
 private:
     /* data */
     int bimodal_table[NUM_CPUS][TAGE_BIMODAL_TABLE_SIZE];  // range 0-3
-    struct entry{
-        uint8_t ctr;  // range 0-7
-        uint16_t tag; 
-        uint8_t useful;  // range 0-3
-    } predictor_table[NUM_CPUS][TAGE_NUM_COMPONENTS][TAGE_PREDICTOR_TABLE_SIZE];
-    
+    struct tage_predictor_table_entry predictor_table[NUM_CPUS][TAGE_NUM_COMPONENTS][TAGE_PREDICTOR_TABLE_SIZE];
     uint8_t global_history[NUM_CPUS][TAGE_HISTORY_LENGTH];
     
 
@@ -27,9 +29,11 @@ public:
     void init(uint32_t cpu);
     uint8_t predict(uint32_t cpu, uint64_t ip);
     void update(uint32_t cpu, uint64_t ip, uint8_t taken);
-    uint16_t get_bimodal_index(uint64_t ip);
-    uint16_t get_predictor_index(uint64_t ip, uint8_t *global_history, int component);
-    uint16_t get_tag(uint64_t ip, uint8_t *global_history, int component);
+    
+    uint16_t get_bimodal_index(uint32_t cpu, uint64_t ip);
+    uint16_t get_predictor_index(uint32_t cpu, uint64_t ip, int component);
+    uint16_t get_tag(uint32_t cpu, uint64_t ip, int component);
+    struct tage_predictor_table_entry* get_match_below_n(uint32_t cpu, uint64_t ip, int component);
 
     Tage(/* args */);
     ~Tage();
@@ -56,18 +60,22 @@ void Tage::update(uint32_t cpu, uint64_t ip, uint8_t taken){
 
 }
 
-uint16_t Tage::get_bimodal_index(uint64_t ip){
+uint16_t Tage::get_bimodal_index(uint32_t cpu, uint64_t ip){
 
 }
 
-uint16_t Tage::get_predictor_index(uint64_t ip, uint8_t *global_history, int component){
+uint16_t Tage::get_predictor_index(uint32_t cpu, uint64_t ip, int component){
 
 }
 
-uint16_t Tage::get_tag(uint64_t ip, uint8_t *global_history, int component){
+uint16_t Tage::get_tag(uint32_t cpu, uint64_t ip, int component){
 
 }
 
+struct tage_predictor_table_entry* Tage::get_match_below_n(uint32_t cpu, uint64_t ip, int component)
+{
+
+}
 
 Tage::Tage()
 {
