ede70dd6f8646958b59a2e231d4589ab6918e86b
Jayesh Singla <jayesh11.3.01@gmail.com>
2021-11-25 21:07:36 +0000

    Update run.sh

--- branch/tage.h
+++ branch/tage.h
@@ -21,25 +21,25 @@ const uint8_t TAGE_TAG_BITS[TAGE_NUM_COMPONENTS] = {7, 7, 8, 8, 9, 10, 11, 12, 1
 
 struct tage_predictor_table_entry
 {
-    uint8_t ctr; // range 0-7
-    Tag tag;
-    uint8_t useful; // range 0-3
+    uint8_t ctr; // The counter on which prediction is based Range - 0-7
+    Tag tag; // Stores the tag
+    uint8_t useful; // Variable to store the usefulness of the entry Range - 0-3
 };
 
 class Tage
 {
 private:
     /* data */
-    int num_branches;
-    uint8_t bimodal_table[TAGE_BIMODAL_TABLE_SIZE]; // range 0-3
+    int num_branches; // Stores the number of branch instructions since the last useful reset
+    uint8_t bimodal_table[TAGE_BIMODAL_TABLE_SIZE]; // Array represent the counters of the bimodal table
     struct tage_predictor_table_entry predictor_table[TAGE_NUM_COMPONENTS][(1 << TAGE_MAX_INDEX_BITS)];
-    uint8_t global_history[TAGE_GLOBAL_HISTORY_BUFFER_LENGTH];
-    uint8_t path_history[TAGE_PATH_HISTORY_BUFFER_LENGTH];
-    uint8_t use_alt_on_na; // 4 bit counter
-    int component_history_lengths[TAGE_NUM_COMPONENTS];
-    uint8_t tage_pred, pred, alt_pred;
-    int pred_comp, alt_comp;
-    int STRONG;
+    uint8_t global_history[TAGE_GLOBAL_HISTORY_BUFFER_LENGTH]; // Stores the global branch history
+    uint8_t path_history[TAGE_PATH_HISTORY_BUFFER_LENGTH]; // Stores the last bits of the last N branch PCs
+    uint8_t use_alt_on_na; // 4 bit counter to decide between alternate and provider component prediction
+    int component_history_lengths[TAGE_NUM_COMPONENTS]; // History lengths used to compute hashes for different components
+    uint8_t tage_pred, pred, alt_pred; // Final prediction , provider prediction, and alternate prediction
+    int pred_comp, alt_comp; // Provider and alternate component of last branch PC
+    int STRONG; //Strength of provider prediction counter of last branch PC
 
 public:
     void init();
@@ -61,6 +61,9 @@ public:
 
 void Tage::init()
 {
+    /*
+    Initializes the member variables
+    */
     use_alt_on_na = 8;
     tage_pred = 0;
     for (int i = 0; i < TAGE_BIMODAL_TABLE_SIZE; i++)
@@ -80,7 +83,7 @@ void Tage::init()
     double power = 1;
     for (int i = 0; i < TAGE_NUM_COMPONENTS; i++)
     {
-        component_history_lengths[i] = int(TAGE_MIN_HISTORY_LENGTH * power + 0.5);
+        component_history_lengths[i] = int(TAGE_MIN_HISTORY_LENGTH * power + 0.5); // set component history lengths
         power *= TAGE_HISTORY_ALPHA;
     }
 
@@ -89,24 +92,28 @@ void Tage::init()
 
 uint8_t Tage::get_prediction(uint64_t ip, int comp)
 {
-    if(comp == 0)
+    /*
+    Get the prediction according to a specific component 
+    */
+    if(comp == 0) // Check if component is the bimodal table
     {
-        Index index = get_bimodal_index(ip);
+        Index index = get_bimodal_index(ip); // Get bimodal index
         return bimodal_table[index] >= (1 << (TAGE_BASE_COUNTER_BITS - 1));
     }
     else
     {
-        Index index = get_predictor_index(ip, comp);
+        Index index = get_predictor_index(ip, comp); // Get component-specific index
         return predictor_table[comp - 1][index].ctr >= (1 << (TAGE_COUNTER_BITS - 1));
     }
 }
 
 uint8_t Tage::predict(uint64_t ip)
 {
-    pred_comp = get_match_below_n(ip, TAGE_NUM_COMPONENTS + 1);
-    alt_comp = get_match_below_n(ip, pred_comp);
+    pred_comp = get_match_below_n(ip, TAGE_NUM_COMPONENTS + 1); // Get the first predictor from the end which matches the PC
+    alt_comp = get_match_below_n(ip, pred_comp); // Get the first predictor below the provider which matches the PC 
 
-    pred = get_prediction(ip, pred_comp);
+    //Store predictions for both components for use in the update step
+    pred = get_prediction(ip, pred_comp); 
     alt_pred = get_prediction(ip, alt_comp);
 
     if(pred_comp == 0)
@@ -115,7 +122,7 @@ uint8_t Tage::predict(uint64_t ip)
     {
         Index index = get_predictor_index(ip, pred_comp);
         STRONG = abs(2 * predictor_table[pred_comp - 1][index].ctr + 1 - (1 << TAGE_COUNTER_BITS)) > 1;
-        if (use_alt_on_na < 8 || STRONG)
+        if (use_alt_on_na < 8 || STRONG) // Use provider component only if USE_ALT_ON_NA < 8 or the provider counter is strong
             tage_pred = pred;
         else
             tage_pred = alt_pred;
@@ -125,6 +132,9 @@ uint8_t Tage::predict(uint64_t ip)
 
 void Tage::ctr_update(uint8_t &ctr, int cond, int low, int high)
 {
+    /*
+    Function to update bounded counters according to some condition
+    */
     if(cond && ctr < high)
         ctr++;
     else if(!cond && ctr > low)
@@ -247,41 +257,55 @@ void Tage::update(uint64_t ip, uint8_t taken)
 
 Index Tage::get_bimodal_index(uint64_t ip)
 {
+    /*
+    Return index of the PC in the bimodal table using the last K bits
+    */
     return ip & (TAGE_BIMODAL_TABLE_SIZE - 1);
 }
 
 Path Tage::get_path_history_hash(int component)
 {
+    /*
+    Use a hash-function to compress the path history
+    */
     Path A = 0;
-    int size = component_history_lengths[component - 1];
+    
+    int size = component_history_lengths[component - 1] > 16 ? 16 : component_history_lengths[component-1]; // Size of hash output
     for (int i = TAGE_PATH_HISTORY_BUFFER_LENGTH - 1; i >= 0; i--)
     {
-        A = (A << 1) | path_history[i];
+        A = (A << 1) | path_history[i]; // Build the bit vector a using the path history array
     }
+
     A = A & ((1 << size) - 1);
     Path A1;
     Path A2;
-    A1 = A & ((1 << TAGE_INDEX_BITS[component - 1]) - 1);
-    A2 = A >> TAGE_INDEX_BITS[component - 1];
+    A1 = A & ((1 << TAGE_INDEX_BITS[component - 1]) - 1); // Get last M bits of A
+    A2 = A >> TAGE_INDEX_BITS[component - 1]; // Get second last M bits of A
+
+    // Use the hashing from the CBP-4 L-Tage submission
     A2 = ((A2 << component) & ((1 << TAGE_INDEX_BITS[component - 1]) - 1)) + (A2 >> abs(TAGE_INDEX_BITS[component - 1] - component));
     A = A1 ^ A2;
     A = ((A << component) & ((1 << TAGE_INDEX_BITS[component - 1]) - 1)) + (A >> abs(TAGE_INDEX_BITS[component - 1] - component));
+    
     return (A);
 }
 
 History Tage::get_compressed_global_history(int inSize, int outSize)
 {
-    History compressed_history = 0;
-    History temporary_history = 0;
+    /*
+    Compress global history of last 'inSize' branches into 'outSize' by wrapping the history
+    */
+    History compressed_history = 0; // Stores final compressed history
+    History temporary_history = 0; // Temorarily stores some bits of history
     int compressed_history_length = outSize;
     for (int i = 0; i < inSize; i++)
     {
         if (i % compressed_history_length == 0)
         {
-            compressed_history ^= temporary_history;
+            compressed_history ^= temporary_history; // XOR current segment into the compressed history
             temporary_history = 0;
         }
-        temporary_history = (temporary_history << 1) | global_history[i];
+        temporary_history = (temporary_history << 1) | global_history[i]; // Build history bit vector
     }
     compressed_history ^= temporary_history;
     return compressed_history;
@@ -289,7 +313,12 @@ History Tage::get_compressed_global_history(int inSize, int outSize)
 
 Index Tage::get_predictor_index(uint64_t ip, int component)
 {
-    Path path_history_hash = get_path_history_hash(component);
+    /*
+    Get index of PC in a particular predictor component
+    */
+    Path path_history_hash = get_path_history_hash(component); // Hash of path history
+
+    // Hash of global history
     History global_history_hash = get_compressed_global_history(component_history_lengths[component - 1], TAGE_INDEX_BITS[component - 1]);
 
     return (global_history_hash ^ ip ^ (ip >> (abs(TAGE_INDEX_BITS[component - 1] - component) + 1)) ^ path_history_hash) & ((1 << TAGE_INDEX_BITS[component-1]) - 1);
@@ -297,25 +326,32 @@ Index Tage::get_predictor_index(uint64_t ip, int component)
 
 Tag Tage::get_tag(uint64_t ip, int component)
 {
+    /*
+    Get tag of a PC for a particular predictor component
+    */
     History global_history_hash = get_compressed_global_history(component_history_lengths[component - 1], TAGE_TAG_BITS[component - 1]);
     global_history_hash ^= get_compressed_global_history(component_history_lengths[component - 1], TAGE_TAG_BITS[component - 1] - 1);
+    
     return (global_history_hash ^ ip) & ((1 << TAGE_TAG_BITS[component - 1]) - 1);
 }
 
 int Tage::get_match_below_n(uint64_t ip, int component)
 {
+    /*
+    Get component number of first predictor which has an entry for the IP below a specfic component number
+    */
     for (int i = component - 1; i >= 1; i--)
     {
         Index index = get_predictor_index(ip, i);
         Tag tag = get_tag(ip, i);
 
-        if (predictor_table[i - 1][index].tag == tag)
+        if (predictor_table[i - 1][index].tag == tag) // Compare tags at a specific index
         {
             return i;
         }
     }
 
-    return 0;
+    return 0; // Default to bimodal in case no match found
 }
 
 Tage::Tage()
--- run.sh
+++ run.sh
@@ -1,43 +1,63 @@
 #!/bin/bash
 
-while getopts "bg" flag
+while getopts "bgr" flag
 do
     case "$flag" in
         b) BUILD="1";;
         g) GENERATE="1";;
+        r) RUN="1";;
     esac
 done
 
 shift $(($OPTIND - 1))
+
 if [ -z $1 ]
 then
-    echo "Usage : ./run.sh [-b BUILD] [-g GENERATE_RESULTS] binary_name"
+    echo "Usage : ./run.sh [-b BUILD] [-g GENERATE_RESULTS] [-r RUN] branch_predictor [binary_name] [results_directory]"
     exit 1
 fi
 
-if [ ! -z $BUILD ]
+bpred=$1
+bin_name=$2
+results_dir=$3
+
+if [ -z $bin_name ]
 then
-    ./build_champsim.sh ltage no no no no lru 1 $1
+    bin_name=$bpred
 fi
 
-pids=""
+if [ -z $results_dir ]
+then
+   results_dir=$bin_name
+fi
 
-for i in 1 2 3 4 9
-do  
-    ./run_champsim.sh $1 10 10 server_00$i.champsimtrace.xz $1 &
-    pids="$pids $!"
-done
 
-for i in {10..39}
-do  
-    ./run_champsim.sh $1 10 10 server_0$i.champsimtrace.xz $1 &
-    pids="$pids $!"
-done
+if [ ! -z $BUILD ]
+then
+    ./build_champsim.sh $bpred no no no no lru 1 $bin_name
+fi
 
-for pid in $pids
-do
-    wait $pid 
-done
+if [ ! -z $RUN ]
+then
+    pids=""
+
+    for i in 1 2 3 4 9
+    do  
+        ./run_champsim.sh $bin_name 10 10 server_00$i.champsimtrace.xz $results_dir &
+        pids="$pids $!"
+    done
+
+    for i in {10..39}
+    do  
+        ./run_champsim.sh $bin_name 10 10 server_0$i.champsimtrace.xz $results_dir &
+        pids="$pids $!"
+    done
+
+    for pid in $pids
+    do
+        wait $pid 
+    done
+fi
 
 if [ ! -z $GENERATE ]
 then
