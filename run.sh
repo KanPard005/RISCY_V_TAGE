7f1a65f816c2dc30080d4d9ab2b57ef2eba180bc
Kanad Pardeshi <kanad5501@gmail.com>
2021-11-25 11:38:11 +0530

    LTAGE improvement

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 pids=""
 # for i in 1 2 3 4 9
-for i in 10 11 12 13 14 15
+for i in 1 2 3 4 9 # {10..39}
 do  
-    ./run_champsim.sh $1 10 10 server_0$i.champsimtrace.xz $1 &
+    ./run_champsim.sh $1 10 10 server_00$i.champsimtrace.xz $1 &
     pids="$pids $!"
 done
 
